d30906e1d0ef21382e2e87095470daafdab46bf9
mohdhasnain786 <102922737+mohdhasnain786@users.noreply.github.com>
2024-02-06 11:51:06 +0530

    Add files via upload

--- 01_Variables.js
+++ 01_Variables.js
@@ -1,1 +1,16 @@
 console.log("Mohammed Hasnain Ahmed Faruk")
+const AccountId = 4327
+let AccountName = "Mohammed Hasnain Ahmed"
+var AccountCity = "Mumbai"
+AccountArea = "Seawoods"
+
+console.table([typeof AccountId, AccountName, AccountCity, AccountArea]);
+console.log(typeof AccountCity); 
+
+// Let, const , var (these are the type of variables )
+// Variables are the name of the locations in the memory
+// Value are the set of content which is located to the variables 
+// Console.log isused to print the content in Parenthesis(if it is in string format then it should be in ''/"")(if it is in      number format it can be written directly)(same for boolean as numbers)
+// Let is use for variables which can't be chnge beyond the scope 
+// Var can be easily chnge in and out of the Scope 
+// Const can not be changes anywhere. 
