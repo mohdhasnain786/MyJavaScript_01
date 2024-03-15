7e6389e08b4467912aab439313c92c164900055c
mohdhasnain786 <102922737+mohdhasnain786@users.noreply.github.com>
2024-03-15 14:33:28 +0530

    Add files via upload

---
+++ 02_DataTypes.js
@@ -0,0 +1,39 @@
+const AccountId = 4327 
+// It is basically a Number Datatype 
+
+let AccountName = "Mohammed Hasnain Ahmed"
+var AccountCity = "Mumbai"
+AccountArea = "Seawoods"
+// String Datatype 
+
+let  AccountLoggedIn = false
+// Boolean Datatype
+
+ BigNumber = 46316840354n
+ // BigInt Datatype which basically contaions a 'n'at  last(which represent a long number or a complicated scientific number) 
+ 
+let cityTemperature = null;
+//null is does not contain any value as such with empty value 
+
+var AccountHolderDrivingLicenseNumber ;
+// Undefined Datatype with does not dontai any value with an empty space 
+
+let AccountPassword = Symbol('12300')
+// these conatin a specialvalue which is unique in its own way and does not match wiht the same value will not be equal as such
+
+
+// Data Types contains many variablestypes such as bigInt, Number, String, Boolean, null, Undefined & Symbol
+ 
+
+// there are other  different types of Datatypes which is non primitive(reference): Array, Object, Function
+
+let NameList = [" Hasnain Ahmed", "Shahabuddin", "Afreen"]
+// Array conti=ain a list which can not be changed 
+
+let SetOfDetails  =  {
+    Name :" Mohammed Hasnain",
+    Age : 22,
+    BirhtPlace : "Mumbai"
+
+}
+// object (set of info withthe scope with a particular Varaiables])
